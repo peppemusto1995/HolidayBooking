a67796c6e122163cc2235c139a1330a34a00baf5
peppemusto1995 <113111697+peppemusto1995@users.noreply.github.com>
2025-03-14 18:11:26 +0100

    Added Spring project

---
+++ backend/holidaybooking/src/test/java/com/example/holidaybooking/HolidayBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holidaybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
